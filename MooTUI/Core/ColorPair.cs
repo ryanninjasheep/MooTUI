0957684f8ede5dadbd90e6ebc7740c3b0953b420
Ryan O-Y <ryanninjasheep@gmail.com>
2020-07-27 17:06:54 -0700

    Moved colorpair and theme out of color

---
+++ MooTUI/Core/ColorPair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MooTUI.Core
+{
+    public struct ColorPair : IEquatable<ColorPair>
+    {
+        public Color Fore { get; }
+        public Color Back { get; }
+
+        public ColorPair(Color fore, Color back)
+        {
+            Fore = fore;
+            Back = back;
+        }
+
+        public ColorPair Invert() => new ColorPair(Back, Fore);
+
+        public override bool Equals(object obj) => obj is ColorPair c ? Equals(c) : false;
+        public override int GetHashCode() => (int)Fore ^ (int)Back;
+
+        public bool Equals(ColorPair other) => Fore == other.Fore && Back == other.Back;
+
+        public static bool operator ==(ColorPair a, ColorPair b) => a.Equals(b);
+        public static bool operator !=(ColorPair a, ColorPair b) => !a.Equals(b);
+    }
+}
